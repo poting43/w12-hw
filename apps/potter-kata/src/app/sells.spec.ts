020a8c33807dabfa22535188bc65bf2101885c87
poting43 <a0918203516@gmail.com>
2022-05-12 16:40:56 +0800

    red_test5

--- apps/potter-kata/src/app/sells.spec.ts
+++ apps/potter-kata/src/app/sells.spec.ts
@@ -32,4 +32,13 @@ describe('Sells', () => {
     sells.buy(3,3);
     expect(sells.price).toBe(730);
   });
+
+  test('Sell 2 for the first episode, 2 for the second episode, 2 for the third episode, 1 for the fourth episode', () => {
+    const sells = new Sells();
+    sells.buy(1,2);
+    sells.buy(2,2);
+    sells.buy(3,2);
+    sells.buy(4,1);
+    expect(sells.price).toBe(590);
+  });
 });
